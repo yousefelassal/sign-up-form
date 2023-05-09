4ec3ce80c270c75ee1d21dfa58e8242f1861f3c0
yousef <yousefsameh24@gmail.com>
2023-05-09 19:14:13 +0300

    add animation json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import lottie from "astro-integration-lottie";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    lottie(),
+  ]
+});
